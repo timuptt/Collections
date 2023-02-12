e707fa588c7e669c3d037af069608bb0879b905d
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-02-12 19:34:30 +0400

    Update ApplicationUser.cs

--- src/Collections/Collections.Infrastructure/Identity/Models/ApplicationUser.cs
+++ src/Collections/Collections.Infrastructure/Identity/Models/ApplicationUser.cs
@@ -1,10 +1,11 @@
+using Collections.ApplicationCore.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace Collections.Infrastructure.Identity.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    public string? FirstName { get; set; }
+    public int UserProfileId { get; set; }
     
-    public string? LastName { get; set; }
+    public UserProfile UserProfile { get; set; }
 }

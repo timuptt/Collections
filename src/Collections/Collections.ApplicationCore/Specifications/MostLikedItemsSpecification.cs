008dce48ebba9b43e0b61b73577d04c35cfb2680
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-02-12 19:25:15 +0400

    Add specifications to read data access

---
+++ src/Collections/Collections.ApplicationCore/Specifications/MostLikedItemsSpecification.cs
@@ -0,0 +1,14 @@
+using Ardalis.Specification;
+using Collections.ApplicationCore.Models;
+
+namespace Collections.ApplicationCore.Specifications;
+
+public class MostLikedItemsSpecification : Specification<Item>, ISingleResultSpecification
+{
+    public MostLikedItemsSpecification()
+    {
+        Query
+            .OrderByDescending(i => i.Likes.Count)
+            .Take(5);
+    }
+}

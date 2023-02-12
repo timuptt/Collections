102de4521f09f6705240dc621dd3a3c381077e44
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-02-12 19:33:55 +0400

    Migrations

---
+++ src/Collections/Collections.Infrastructure/Migrations/20230211165617_RemoveFkFromUserProfile.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Collections.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFkFromUserProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_ApplicationUserId",
+                table: "UserProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_ApplicationUserId",
+                table: "UserProfiles");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "081065ba-cd04-40a4-9bb7-4b82d1651454");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "096883d7-4cb0-49f3-ba34-a52932d9abda");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "85e743aa-34a5-430d-96fb-ac92e6058d06");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "037718ed-91d1-4874-b9b1-288b398373bc", null, "Admin", "ADMIN" },
+                    { "0923bbba-ccf6-4153-b139-5e88b78fdff4", null, "Author", "AUTHOR" },
+                    { "3b54d9c1-e723-4bb7-adb0-789a1f621a94", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "037718ed-91d1-4874-b9b1-288b398373bc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0923bbba-ccf6-4153-b139-5e88b78fdff4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3b54d9c1-e723-4bb7-adb0-789a1f621a94");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "081065ba-cd04-40a4-9bb7-4b82d1651454", null, "Admin", "ADMIN" },
+                    { "096883d7-4cb0-49f3-ba34-a52932d9abda", null, "User", "USER" },
+                    { "85e743aa-34a5-430d-96fb-ac92e6058d06", null, "Author", "AUTHOR" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_ApplicationUserId",
+                table: "UserProfiles",
+                column: "ApplicationUserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_ApplicationUserId",
+                table: "UserProfiles",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

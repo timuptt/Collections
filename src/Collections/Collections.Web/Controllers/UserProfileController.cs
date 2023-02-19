9f724ef6670fffbf9e14009b0b30d31c91f31b56
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-02-20 00:23:59 +0400

    Implement generic paginated list and edit specification for filtering

--- src/Collections/Collections.Web/Controllers/UserProfileController.cs
+++ src/Collections/Collections.Web/Controllers/UserProfileController.cs
@@ -1,6 +1,7 @@
 using Collections.ApplicationCore.Models;
 using Collections.ApplicationCore.Specifications;
 using Collections.Shared.Interfaces;
+using Collections.Web.Models;
 using Collections.Web.Models.UserProfile;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,12 @@ namespace Collections.Web.Controllers;
 public class UserProfileController : Controller
 {
     private readonly IReadRepository<UserProfile> _userProfileReadRepository;
+    private readonly IReadRepository<UserCollection> _userCollectionsReadRepository;
 
-    public UserProfileController(IReadRepository<UserProfile> userProfileReadRepository)
+    public UserProfileController(IReadRepository<UserProfile> userProfileReadRepository, IReadRepository<UserCollection> userCollectionsReadRepository)
     {
         _userProfileReadRepository = userProfileReadRepository;
+        _userCollectionsReadRepository = userCollectionsReadRepository;
     }
 
     // GET
@@ -21,10 +24,15 @@ public class UserProfileController : Controller
         return View();
     }
 
-    public async Task<IActionResult> Profile(int id)
+    public async Task<IActionResult> Profile(int id, int pageNumber = 1, int pageSize = 10)
     {
-        var specification = new UserProfileByIdSpec(id);
-        var profileViewModel = await _userProfileReadRepository.GetBySpecProjectedAsync<UserProfileViewModel>(specification);
+        var profileSpec = new UserProfileByIdSpec(id);
+        var profileViewModel = await _userProfileReadRepository.GetBySpecProjectedAsync<UserProfileViewModel>(profileSpec);
+        var collectionsSpec = new UserCollectionsByProfileIdFilteredPaginatedSpec(id, pageSize, pageNumber - 1);
+        var countSpec = new UserCollectionsCountByProfileIdSpec(id);
+        profileViewModel.UserCollections =
+            await PaginatedList<CollectionForUserProfileViewModel, UserCollection>.CreateAsync(_userCollectionsReadRepository,
+                collectionsSpec, countSpec, pageNumber, pageSize);
         return View(profileViewModel);
     }
 }

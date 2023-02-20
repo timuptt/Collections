d7ce4ed18853ee757155d817509926947afb982c
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-02-20 23:21:35 +0400

    Add filter to dependencies

--- src/Collections/Collections.Web/Configuration/DependencyInjection.cs
+++ src/Collections/Collections.Web/Configuration/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using Collections.ApplicationCore.Common.Mappings;
 using Collections.ApplicationCore.Interfaces;
+using Collections.ApplicationCore.Models;
 using Collections.ApplicationCore.Services;
 using Collections.Infrastructure.Data.Contexts;
 using Collections.Infrastructure.Data.Repositories;
@@ -9,6 +10,7 @@ using Collections.Infrastructure.Identity.Models;
 using Collections.Shared.Interfaces;
 using Collections.Web.Common.Mappings;
 using Collections.Web.Configuration.Connection;
+using Collections.Web.Filters;
 using Collections.Web.Interfaces;
 using Collections.Web.Services;
 using Microsoft.AspNetCore.Identity;
@@ -61,6 +63,7 @@ public static class DependencyInjection
                 configuration.AddProfile(new AssemblyMappingProfile(Assembly.GetAssembly(typeof(IMapWith<>))));
             }
                 );
+        services.AddScoped<IFilter<UserCollection>, CollectionFilter>();
         return services;
     }
 }

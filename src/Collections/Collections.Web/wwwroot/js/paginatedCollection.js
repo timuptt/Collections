69c0c6413e4d311b28960aec107d69e0b40294bd
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-03-03 08:21:29 +0400

    Fixed comments bg, collection description in table, add logos

--- src/Collections/Collections.Web/wwwroot/js/paginatedCollection.js
+++ src/Collections/Collections.Web/wwwroot/js/paginatedCollection.js
@@ -10,8 +10,8 @@ $(function() {
             datatype: 'application/json'
         },
         columns: [
-            { data: "id", autoWidth: true,
-                render : function(data, type) {
+            { data: "id", autoWidth: true, name: "Id",
+                mRender : function(data, type, row) {
                     if(type === 'display') {
                         return $('<a>')
                             .attr('href', document.location.origin + "/Collections/Details/" + data)
@@ -23,7 +23,21 @@ $(function() {
                     return data
                 }},
             { data: "title", name: "Title", autoWidth: true },
-            { data: "description", name: "Description", autoWidth: true },
+            { data: "description", name: "Description", autoWidth: true,
+                mRender: function (data, type, row){
+                    if (data.length > 50){
+                        return $('<span>')
+                            .attr("data-bs-toggle", "tooltip")
+                            .attr("data-bs-placement", "top")
+                            .attr("data-bs-title", data)
+                            .text(data.substring(0, 50) + "...")
+                            .wrap('<div></div>')
+                            .parent()
+                            .html();
+                    }
+                    return data;
+                    }
+            },
             { data: "userCollectionTheme", name: "Theme", autoWidth: true },
             { data: "profileFullName", name: "Author", autoWidth: true },
             { data: "itemsCount", name: "Items count", autoWidth: true },
--- src/Collections/Collections.Web/wwwroot/js/site.js
+++ src/Collections/Collections.Web/wwwroot/js/site.js
@@ -2,16 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
 
 $(document).ready(function () {
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    
     $('#itemsTable').DataTable();
 });

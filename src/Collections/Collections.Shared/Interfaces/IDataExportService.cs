cfa2d9de2aa592a87df219793422a5968961ae66
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-03-04 09:40:22 +0400

    Remove unused interface

--- src/Collections/Collections.Shared/Interfaces/IDataExportService.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-using Collections.ApplicationCore.Models;
-
-namespace Collections.Shared.Interfaces;
-
-public interface IDataExportService<T> where T : class 
-{
-    public Task<MemoryStream> ExportToCsw(T dataObject);
-}
